6d34fdbcf5d85373057839e5015d2f2bb9888ab8
eiuthanghuynh <thang.huynhle.cit22@eiu.edu.vn>
2025-10-22 15:27:47 +0700

    Test: Implement unit tests for TodoService

--- tests/unit/model.test.js
+++ tests/unit/model.test.js
@@ -14,6 +14,12 @@ describe('TodoService Unit Tests', () => {
         // TODO: Call the addTodo method with some text.
         // Then, assert that the service's todos array has a length of 1.
         // Assert that the text of the first todo matches the input text.
+        text = "test text";
+        
+        service.addTodo(text);
+
+        expect(service.todos.length).toBe(1);
+        expect(service.todos[0].text).toMatch(text);
     });
 
     test('should toggle the completed state of a todo', () => {
